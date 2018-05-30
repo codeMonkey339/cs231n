fixed the forward pass of the batch normalization

assign2/spring1718_assignment2_v2/cs231n/layers.py
@@ -162,7 +162,6 @@ def batchnorm_forward(x, gamma, beta, bn_param):
     N, D = x.shape
     running_mean = bn_param.get('running_mean', np.zeros(D, dtype=x.dtype))
     running_var = bn_param.get('running_var', np.zeros(D, dtype=x.dtype))
-
     out, cache = None, None
     if mode == 'train':
         #######################################################################
@@ -191,10 +190,8 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         x = (x-sample_mean)/np.sqrt(sample_variance + eps)
         out = x * beta + gamma
         # need to add cache variables
-        bn_param['running_mean'] = momentum * running_mean \
-                                   + momentum * sample_mean
-        bn_param['running_var'] = momentum * running_var \
-                                  + (1-momentum * sample_variance)
+        running_mean = momentum * running_mean + (1-momentum) * sample_mean
+        running_var = momentum * running_var + (1-momentum) * sample_variance
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -205,7 +202,8 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # then scale and shift the normalized data using gamma and beta.      #
         # Store the result in the out variable.                               #
         #######################################################################
-        pass
+        x_hat = (x - running_mean)/np.sqrt(running_var+eps)
+        out = gamma * x_hat + beta
         #######################################################################
         #                          END OF YOUR CODE                           #
         #######################################################################